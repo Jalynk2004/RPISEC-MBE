3af6fac2aba0eb809613a94a49b5cf7c21915d8b
Jalynk2004 <81233886+Jalynk2004@users.noreply.github.com>
2023-03-09 21:37:54 +0700

    Create lab8C.sh

---
+++ lab08/lab8C.sh
@@ -0,0 +1,9 @@
+0: stdin
+1: stdout
+2: stderr
+
+The first 3 non-negative numbers is fixed to descripe the 3 file descriptors. When opening a new file, fd will start from 3 and so on.
+
+lab8C -fn=/home/lab8B/.pass -fd=3 (open /home/lab8B/.pass)
+
+#3v3ryth1ng_Is_@_F1l3

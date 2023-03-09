20b8d2cb2cfc674ac11c0872013ae5ee9b525455
Jalynk2004 <81233886+Jalynk2004@users.noreply.github.com>
2023-03-09 21:38:19 +0700

    Update lab8C.sh

--- lab08/lab8C.sh
+++ lab08/lab8C.sh
@@ -2,7 +2,7 @@
 1: stdout
 2: stderr
 
-The first 3 non-negative numbers is fixed to descripe the 3 file descriptors. When opening a new file, fd will start from 3 and so on.
+The first 3 non-negative numbers is fixed to describe the 3 file descriptors. When opening a new file, fd will start from 3 and so on.
 
 lab8C -fn=/home/lab8B/.pass -fd=3 (open /home/lab8B/.pass)
 
